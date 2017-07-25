8b18141b2b809cd3383acc6d64fbe185140fcb91
verixx <abdurraqeeb53@gmail.com>
2017-07-25 19:43:42 +1000

    Improved bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ import random
 
 
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
@@ -38,7 +38,10 @@ startup_extensions = [
     'cogs.robolog',
     'cogs.tags',
     'cogs.setup',
-    'cogs.levels'
+    'cogs.levels',
+    'cogs.clashroyale',
+    'cogs.repl',
+    'cogs.alias'
 ]
 
 Client = discord.Client()
@@ -68,10 +71,7 @@ async def on_ready():
     print('Author: verix')
     print("ID: {}".format(bot.user.id))
     print('DV: {}'.format(discord.__version__))
-    print('------------------------------------')
-
-
-
+    bot.uptime = datetime.datetime.now()
 
 
 
@@ -84,18 +84,61 @@ def is_owner():
 
 @bot.event
 async def on_member_join(member):
+    with open('cogs/utils/t_config.json') as f:
+        data = json.loads(f.read())
     server = member.server
-    role = 'Larry'
-    fmt = 'Welcome {0.mention} to {1.name}!'
-    autorole = discord.utils.get(server.roles,name=role)
-    await bot.send_message(server, fmt.format(member, server))
-    await bot.add_roles(member,autorole)
+    status = data[server.id]["welcome"]["status"]
+    if status:
+        fmt = data[server.id]["welcome"]["msg"]
+        channel = data[server.id]["welcome"]["channel"]
+        if channel == 'default':
+            channel = member.server
+        else:
+            channel = discord.utils.get(server.channels, id=channel)
+
+        await bot.send_message(channel, fmt.format(member, server))
+
+    autorole = data[server.id]["autorole"]
+    autorole = discord.utils.get(server.roles,id=autorole)
+
+    try:
+        await bot.add_roles(member,autorole)
+    except:
+        pass
+
+@bot.event
+async def on_command(command, ctx):
+    print('------------------------------------')
+    print('Command > {}{} < invoked with > {} <\nServer: {} | {}\nUser: {} | {}'
+        .format(ctx.prefix,
+            command,
+            ctx.invoked_with,
+            ctx.message.server.name, 
+            ctx.message.server.id, 
+            ctx.message.author.name, 
+            ctx.message.author.id))
 
 @bot.event
 async def on_member_remove(member):
     server = member.server
-    fmt = '{0.name} has just left the server.'.format(member)
-    await bot.send_message(server, fmt)
+    with open('cogs/utils/t_config.json') as f:
+        data = json.loads(f.read())
+    status = data[server.id]["leave"]["status"]
+    if status:
+        msg = data[server.id]["leave"]["msg"]
+        channel = data[server.id]['leave']['channel']
+        if channel == 'default':
+            channel = server
+        else:
+            channel = discord.utils.get(server.channels, id=channel)
+
+        await bot.send_message(channel, msg.format(member, server))
+
+@bot.event
+async def on_server_join(server):
+    await bot.send_message(server,
+        "**Hello!** *Thanks for inviting me to your server!*"
+        " **Get started with:** `.config set` and `.help`")
 
 
 async def send_cmd_help(ctx):
@@ -103,30 +146,36 @@ async def send_cmd_help(ctx):
         pages = bot.formatter.format_help_for(ctx, ctx.invoked_subcommand)
         for page in pages:
             await bot.send_message(ctx.message.channel, page)
+        print('Sent command help')
     else:
         pages = bot.formatter.format_help_for(ctx, ctx.command)
         for page in pages:
             await bot.send_message(ctx.message.channel, page)
+        print('Sent command help')
 
 @bot.event
 async def on_command_error(error, ctx):
- channel = ctx.message.channel
- if isinstance(error, commands.MissingRequiredArgument):
-     await send_cmd_help(ctx)
- elif isinstance(error, commands.BadArgument):
-     await send_cmd_help(ctx)
- elif isinstance(error, commands.DisabledCommand):
-     await bot.send_message(channel, "That command is disabled.")
- elif isinstance(error, commands.CommandInvokeError):
-     # A bit hacky, couldn't find a better way
-     no_dms = "Cannot send messages to this user"
-     is_help_cmd = ctx.command.qualified_name == "help"
-     is_forbidden = isinstance(error.original, discord.Forbidden)
-     if is_help_cmd and is_forbidden and error.original.text == no_dms:
-         msg = ("I couldn't send the help message to you in DM. Either"
-                " you blocked me or you disabled DMs in this server.")
-         await bot.send_message(channel, msg)
-         return
+   print(error)
+   channel = ctx.message.channel
+   if isinstance(error, commands.MissingRequiredArgument):
+       await send_cmd_help(ctx)
+       print('Sent command help')
+   elif isinstance(error, commands.BadArgument):
+       await send_cmd_help(ctx)
+       print('Sent command help')
+   elif isinstance(error, commands.DisabledCommand):
+       await bot.send_message(channel, "That command is disabled.")
+       print('Command disabled.')
+   elif isinstance(error, commands.CommandInvokeError):
+       # A bit hacky, couldn't find a better way
+       no_dms = "Cannot send messages to this user"
+       is_help_cmd = ctx.command.qualified_name == "help"
+       is_forbidden = isinstance(error.original, discord.Forbidden)
+       if is_help_cmd and is_forbidden and error.original.text == no_dms:
+           msg = ("I couldn't send the help message to you in DM. Either"
+                  " you blocked me or you disabled DMs in this server.")
+           await bot.send_message(channel, msg)
+           return
 
 @bot.command(pass_context=True,name='cog')
 @owner_only()
@@ -178,12 +227,11 @@ async def servers(ctx):
 
 
 
-
 if __name__ == "__main__":
     for extension in startup_extensions:
         try:
             bot.load_extension(extension)
-            print('Succesfully loaded extenstion: {}'.format(extension))
+            print('Loaded extension: {}'.format(extension))
         except Exception as e:
             exc = '{}: {}'.format(type(e).__name__, e)
             print('Failed to load extension {}\n{}'.format(extension, exc))
