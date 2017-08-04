c8863e72bd9aecb34618c9470d1964c0e515bfee
verixx <abdurraqeeb53@gmail.com>
2017-08-05 08:46:48 +1000

    Made the presence command more clear.

--- bot.py
+++ bot.py
@@ -229,23 +229,23 @@ async def _reload(ctx,*, module : str):
     else:
         x = await bot.edit_message(x,'Done. \N{OK HAND SIGN}')
 
-@bot.command(name='presence',hidden=True)
-@is_owner()
+@bot.command(name='presence')
 async def _set(Type=None,*,thing=None):
-        if Type is None:
-                await bot.say('Usage: `.presence [game/stream] [message]`')
+    """Change the bot's discord game/stream!"""
+    if Type is None:
+            await bot.say('Usage: `.presence [game/stream] [message]`')
+    else:
+        if Type.lower() == 'stream':
+            await bot.change_presence(game=discord.Game(name=thing,type=1,url='https://www.twitch.tv/a'),status='online')
+            await bot.say('Set presence to. `Streaming {}`'.format(thing))
+        elif Type.lower() == 'game':
+            await bot.change_presence(game=discord.Game(name=thing))
+            await bot.say('Set presence to `Playing {}`'.format(thing))
+        elif Type.lower() == 'clear':
+            await bot.change_presence(game=None)
+            await bot.say('Cleared Presence')
         else:
-                if Type.lower() == 'stream':
-                        await bot.change_presence(game=discord.Game(name=thing,type=1,url='https://www.twitch.tv/a'),status='online')
-                        await bot.say('Done.')
-                elif Type.lower() == 'game':
-                        await bot.change_presence(game=discord.Game(name=thing))
-                        await bot.say('Done.')
-                elif Type.lower() == 'clear':
-                        await bot.change_presence(game=None)
-                        await bot.say('Done.')
-                else:
-                        await bot.say('Usage: `.presence [game/stream] [message]`')
+            await bot.say('Usage: `.presence [game/stream] [message]`')
 
 @bot.command(pass_context=True)
 @is_owner()
