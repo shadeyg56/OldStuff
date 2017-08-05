1ef624ee20fdaf10958727dfc446e1d356a326be
verixx <abdurraqeeb53@gmail.com>
2017-08-05 14:58:24 +1000

    Fixed members_messaged ++

--- cogs/mod.py
+++ cogs/mod.py
@@ -254,7 +254,7 @@ class Mod():
                         print(member)
                     except:
                         print(member, "has DM's turned off")
-                    members_messaged++ # I know this works in java, not clear with python, just want to add 1 to members_messaged each time the loop is run            
+                    members_messaged += 1 # I know this works in java, not clear with python, just want to add 1 to members_messaged each time the loop is run            
             await self.bot.say("Done. " + members_messaged + " members were DMed. (Prepare to have some angry people on your heels...)")
         else:
             await self.bot.say('Server owner only.')
