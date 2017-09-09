2d4f15a84a8add7e5e8ee96132d32436aaeed30e
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-09-09 10:46:00 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -300,23 +300,6 @@ if __name__ == "__main__":
         except Exception as e:
             exc = '{}: {}'.format(type(e).__name__, e)
             print('Error on load: {}\n{}'.format(extension, exc))
-           
-@bot.command(pass_context = True)
-async def ping(self, ctx):
-    msgtime = ctx.message.timestamp.now()
-    await (await self.bot.ws.ping())
-    now = datetime.datetime.now()
-    ping = now - msgtime
-    pong = discord.Embed(title='Pong! Response Time:', description=str(ping.microseconds / 1000.0) + ' ms',
-                        color=FF3DFE)
-    await self.bot.send_message(ctx.message.channel, embed=pong)
-    await self.bot.add_reaction(pong, '\U0001f3d3')   
-    
-@bot.command(pass_context = True)
-async def say(self,ctx, message: str):
-    '''Say something as the bot'''
-    await self.bot.say(message)
-    await self.bot.delete_message(ctx.message)
    
 
 
