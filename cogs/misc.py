dc6363176ca1f85d58b8b95a0ec98b45b176b529
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-09-09 07:24:45 -0500

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -10,9 +10,9 @@ def __init__(self, bot):
     
 @commands.command(pass_context = True)
 async def ping(self, ctx):
-    msgtime = ctx.message.timestamp.now()
+    msgtime = ctx.message.timestamp.now
     await (await self.bot.ws.ping)
-    now = datetime.datetime.now()
+    now = datetime.datetime.now
     ping = now - msgtime
     pong = discord.Embed(title='Pong! Response Time:', description=str(ping.microseconds / 1000.0) + ' ms', color=FF3DFE)
     await self.bot.send_message(ctx.message.channel, embed=pong)
