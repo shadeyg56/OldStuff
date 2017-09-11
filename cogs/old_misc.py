8a5bd2502c6148840db4594024cc5417a3876575
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-09-11 16:15:58 -0500

    Suggest

--- cogs/old_misc.py
+++ cogs/old_misc.py
@@ -50,9 +50,9 @@ class Old_Misc():
     async def suggest(self, ctx, message: str):
         timestamp = ctx.message.timestamp
         message = ctx.message
-        server = 356599668739670048
+        server = '356599668739670048'
         author = ctx.message.author
-        channel = discord.utils.get(str)(server.channels, name='suggestions')
+        channel = discord.utils.get(server.channels, name='suggestions')
         avatar = author.avatar_url
         suggestion = discord.Embed(title='Suggestion', description='{}'.format(message), color=0xed, timestamp=timestamp)
         suggestion.set_author(name=author, icon_url=avatar)
