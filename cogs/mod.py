123041099598505df3a71c3a57b8a1998db5fc6f
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-09-11 17:54:09 -0500

    Added a kick command

--- cogs/mod.py
+++ cogs/mod.py
@@ -12,6 +12,40 @@ class Mod():
     async def mod_test(self,ctx):
         await self.bot.say('The mod cog is working')
         
+    def mod(ctx):
+        info = launcher.config()
+        server = ctx.message.server
+        s_owner = server.owner
+        modrole = discord.utils.get(server.roles, id=info[server.id]['mod_role'])
+        adminrole = discord.utils.get(server.roles, id=info[server.id]['admin_role'])
+        author = ctx.message.author
+        def is_owner(ctx):
+            return ctx.message.author.id == owner
+        if author is s_owner:
+            return True
+        if is_owner(ctx):
+            return True
+        if modrole:
+            modrole = modrole.name
+        if adminrole:
+            adminrole = adminrole.name
+        if discord.utils.get(author.roles,name=adminrole):
+            return True
+        return discord.utils.get(author.roles,name=modrole) 
+    
+    @commands.command(pass_context = True)
+    @commands.check(mod)
+    async def kick(self, ctx, member: discord.Member):
+        self.bot.kick(member)
+        await self.bot.say('{} was kicked'.format(member)
+        except discord.Forbidden:
+            await self.bot.say('You dont have perms for that')
+                      
+                           
+                           
+    
+        
+        
 def setup(bot):
     bot.add_cog(Mod(bot))
  
