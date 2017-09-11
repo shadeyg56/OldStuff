8d701093c6f82d889dfb3f012ebb91774ac582f3
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-09-10 21:09:14 -0500

    Fixed another syntax error on the suggest command

--- cogs/old_misc.py
+++ cogs/old_misc.py
@@ -44,7 +44,7 @@ class Old_Misc():
     async def invite(self, ctx):
         await self.bot.say('**Darkness Invite:** https://discordapp.com/oauth2/authorize?client_id=355189919410421760&scope=bot&permissions=66186303')
                       
-    @commands.command(pass_context = True
+    @commands.command(pass_context = True)
     async def suggest(self, ctx, message: str):
         author = ctx.message.author
         avatar = author.avatar_url
