57b2fc3b22dcb9a6e56c5c78a23c557a5b801eb7
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-09-11 20:59:33 -0500

    Update ping command

--- cogs/old_misc.py
+++ cogs/old_misc.py
@@ -32,7 +32,7 @@ class Old_Misc():
         pingtime = time.time()
         ping = time.time() - pingtime
         pong = discord.Embed(title='Pong! Response Time:', description=str(ping, color=FF3DFE)
-        await self.bot.send_message(ctx.message.channel, embed=pong)
+        await self.bot.say(embed=pong)
         await self.bot.add_reaction(pong, '\U0001f3d3')
         
     @commands.command(pass_context = True)
