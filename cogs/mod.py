2ab0c2717609478659a5943fa3ed3489c7ab82c4
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-09-11 21:58:37 -0500

    Change else to elif

--- cogs/mod.py
+++ cogs/mod.py
@@ -39,7 +39,7 @@ class Mod():
         if ctx.message.author.server_permissions.kick_members:
             await self.bot.kick(member)
             await self.bot.say('{} was kicked'.format(member))
-            else:
+            elif:
                 await self.bot.say('You dont have perms for that')
         
                       
