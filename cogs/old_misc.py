9e535804b2d158bd35ba506bfc733d0a1ea189dc
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-09-11 20:53:21 -0500

    Fixed the ping command (Thanks SpiceThisUp)

--- cogs/old_misc.py
+++ cogs/old_misc.py
@@ -29,10 +29,8 @@ class Old_Misc():
     
     @commands.command(pass_context = True)
     async def ping(self,ctx):
-        msgtime = ctx.message.timestamp.now()
-        await (await self.bot.ws.ping())
-        now = datetime.datetime.now()
-        ping = now - msgtime
+        pingtime = time.time()
+        ping = time.time() - pingtime
         pong = discord.Embed(title='Pong! Response Time:', description=str(ping.microseconds / 1000.0) + ' ms', color=FF3DFE)
         await self.bot.send_message(ctx.message.channel, embed=pong)
         await self.bot.add_reaction(pong, '\U0001f3d3')
